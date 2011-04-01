87e446e7928343a8bd8639d5b23487410ede7a21
jbryer (Research Associate ) <jason@bryer.org>
2011-04-01 17:37:22 +0000

    Added documentation and stepAIC option for logistic regression.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multilevelpsa@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/multilevelPSA/R/logestic.R
+++ pkg/multilevelPSA/R/logestic.R
@@ -5,15 +5,23 @@ NA
 #' 
 #' TODO: Need more details
 #' 
-#' @return a list of glm classes for each level 2
+#' @param vars data frame containing the variables to estimate the logistic regression
+#' @param formula the logistic regression formula to use
+#' @param level2 the name of the column containing the level 2 specification
+#' @param stepAIC if true, stepAIC will be called for each level.
+#' @return a list of glm classes for each level 2 or stepwise-selected model if stepAIC is true.
 #' @export multilevelLR
-multilevelLR <- function(vars, formula, level2) {
-	#TODO: look at including stepAIC in the MASS package
+multilevelLR <- function(vars, formula, level2, stepAIC=FALSE) {
 	lrPlyr <- function(x) {
 		excludeVars = names(x) %in% c(level2)
 		x = x[,!excludeVars]
 		lr = glm(formula, data=x, family=binomial)
-		return(lr)
+		if(stepAIC) {
+			step = stepAIC(lr, trace=FALSE)
+			return(step)
+		} else {
+			return(lr)
+		}
 	}
 	lr.results = dlply(vars, level2, lrPlyr, .progress='text')
 	return(lr.results)
